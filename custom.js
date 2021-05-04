1371165008d1485a319b5fd0eed9d520cc8406e4
r3trosteve <stevebschofield@gmail.comD>
2021-05-04 18:23:09 +0100

    use innerHTML badness

--- custom.js
+++ custom.js
@@ -1,2 +1,5 @@
-console.log('Custom JS Code loaded');
-console.log(eval('2 + 2'));
+(function () {
+    console.log('Custom JS Code loaded');
+    console.log(eval('2 + 2'));
+    document.getElementById('title').innerHTML('New Title Here');  
+})();
